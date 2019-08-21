33fe3d6735627d6d8980c76c7a7308d6ccce82ac
PasanT9 <pasan96tennakoon@gmail.com>
2019-08-21 16:16:32 +0530

    n

--- Peer.java
+++ Peer.java
@@ -20,8 +20,8 @@ public class Peer
         // server is listening on port 5056
         ServerSocket ss = new ServerSocket(5056);
 
-        while (true)
-        {
+        //while (true)
+        //{
             Socket s = null;
 
             try
@@ -49,9 +49,119 @@ public class Peer
                 s.close();
                 e.printStackTrace();
             }
-        }
+        //}
     }
 }
+
+/*
+ * CO 324 - Network and Web Application Design
+ * Assignment - Sample Code
+ */
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+
+
+
+public class RecordPlayback {
+
+boolean stopCapture = false;
+ByteArrayOutputStream byteArrayOutputStream;
+AudioFormat audioFormat;
+TargetDataLine targetDataLine;
+AudioInputStream audioInputStream;
+SourceDataLine sourceDataLine;
+byte tempBuffer[] = new byte[500];
+
+
+
+
+private AudioFormat getAudioFormat() {
+    float sampleRate = 16000.0F;
+    int sampleSizeInBits = 16;
+    int channels = 2;
+    boolean signed = true;
+    boolean bigEndian = true;
+    return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+}
+
+
+
+private void captureAudio() {
+
+    try {
+        Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();    //get available mixers
+        System.out.println("Available mixers:");
+        Mixer mixer = null;
+        for (int cnt = 0; cnt < mixerInfo.length; cnt++) {
+            System.out.println(cnt + " " + mixerInfo[cnt].getName());
+            mixer = AudioSystem.getMixer(mixerInfo[cnt]);
+
+            Line.Info[] lineInfos = mixer.getTargetLineInfo();
+            if (lineInfos.length >= 1 && lineInfos[0].getLineClass().equals(TargetDataLine.class)) {
+                System.out.println(cnt + " Mic is supported!");
+                break;
+            }
+        }
+
+        audioFormat = getAudioFormat();     //get the audio format
+        DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+
+        targetDataLine = (TargetDataLine) mixer.getLine(dataLineInfo);
+        targetDataLine.open(audioFormat);
+        targetDataLine.start();
+
+        DataLine.Info dataLineInfo1 = new DataLine.Info(SourceDataLine.class, audioFormat);
+        sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo1);
+        sourceDataLine.open(audioFormat);
+        sourceDataLine.start();
+
+        //Setting the maximum volume
+        FloatControl control = (FloatControl)sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
+        control.setValue(control.getMaximum());
+
+        captureAndPlay(); //playing the audio
+
+    } catch (LineUnavailableException e) {
+        System.out.println(e);
+        System.exit(0);
+    }
+
+}
+
+    private void captureAndPlay() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        stopCapture = false;
+        try {
+            int readCount;
+            while (!stopCapture) {
+                readCount = targetDataLine.read(tempBuffer, 0, tempBuffer.length);  //capture sound into tempBuffer
+                if (readCount > 0) {
+                    byteArrayOutputStream.write(tempBuffer, 0, readCount);
+                    sourceDataLine.write(tempBuffer, 0, 500);   //playing audio available in tempBuffer
+                }
+            }
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+
+}
+
 
 // ClientHandler class
 class ClientHandler extends Thread
@@ -61,7 +171,7 @@ class ClientHandler extends Thread
     final DataOutputStream dos;
     final Socket s;
     final boolean listen;
-    Queue<Byte[]> buffer = new LinkedList<>();
+    static Queue<byte[]> buffer = new LinkedList<>();
 
 
     // Constructor
@@ -72,6 +182,13 @@ class ClientHandler extends Thread
         this.dis = dis;
         this.dos = dos;
     }
+    public synchronized void addToBuffer(byte[] data){
+      System.out.println("Add to Buffer");
+      buffer.add(data);
+    }
+    public synchronized byte[] getFromBuffer(){
+      return buffer.poll();
+    }
 
     @Override
     public void run()
@@ -79,111 +196,75 @@ class ClientHandler extends Thread
         if(this.listen==true){
           while(true){
             try{
-
+              byte[] audioData=null;
+              int length = dis.readInt();                    // read length of incoming message
+              if(length>0) {
+                  audioData = new byte[length];
+                  dis.readFully(audioData, 0, audioData.length); // read the message
+              }
+              System.out.println("Recieved data");
+              System.out.println(audioData);
+              addToBuffer(audioData);
             }
-            catch(Exception e){
+            catch(IOException e){
 
             }
           }
-        }
 
-        AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
-        AudioInputStream audioInputStream;
-        SourceDataLine sourceDataLine;
-        while (true)
-        {
-            try {
-
-                // Ask user what he wants
-                //dos.writeUTF("What do you want?[Date | Time]..\n"+
-                  //          "Type Exit to terminate connection.");
-
-                // receive the answer from client
-                ///received = dis.read();
-                //byte audioData[] = dis.read
-                byte[] audioData=null;
-                int length = dis.readInt();                    // read length of incoming message
-                if(length>0) {
-                    audioData = new byte[length];
-                    dis.readFully(audioData, 0, audioData.length); // read the message
-                }
-                System.out.println("Recieved data");
-                System.out.println(audioData);
-                try{
-                  InputStream byteArrayInputStream = new ByteArrayInputStream(
-                          audioData);
-                  audioInputStream = new AudioInputStream(byteArrayInputStream,format, audioData.length / format.getFrameSize());
-                  DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
-                  sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-                  sourceDataLine.open(format);
-                  sourceDataLine.start();
-                  int cnt = 0;
-                  byte tempBuffer[] = new byte[500];
-                  try {
-                      while ((cnt = audioInputStream.read(tempBuffer, 0,tempBuffer.length)) != -1) {
-                          if (cnt > 0) {
-                              // Write data to the internal buffer of
-                              // the data line where it will be
-                              // delivered to the speaker.
-                              sourceDataLine.write(tempBuffer, 0, cnt);
-                          }// end if
-                      }
-                  } catch (IOException e) {
-                      e.printStackTrace();
-                  }
-                  // Block and wait for internal buffer of the
-                  // data line to empty.
-                  sourceDataLine.drain();
-                  sourceDataLine.close();
-
-                }
-                catch(Exception e){
-
-                }
-
-                if(false)
-                {
-                    System.out.println("Client " + this.s + " sends exit...");
-                    System.out.println("Closing this connection.");
-                    this.s.close();
-                    System.out.println("Connection closed");
-                    break;
-                }
-
-                // creating Date object
-              //  Date date = new Date();
-
-                // write on output stream based on the
-                // answer from the client
-              /*  switch (received) {
-
-                    case "Date" :
-                        toreturn = fordate.format(date);
-                        dos.writeUTF(toreturn);
-                        break;
-
-                    case "Time" :
-                        toreturn = fortime.format(date);
-                        dos.writeUTF(toreturn);
-                        break;
-
-                    default:
-                        dos.writeUTF("Invalid input");
-                        break;
-                }*/
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        try
-        {
-            // closing resources
+          /*try {
+            this.s.close();
             this.dis.close();
             this.dos.close();
-
-        }catch(IOException e){
+          }
+          catch(IOException e){
             e.printStackTrace();
+          }*/
+
         }
+        else {
+          AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+          AudioInputStream audioInputStream;
+          SourceDataLine sourceDataLine;
+          while(true){
+            //System.out.println("Playing");
+            try{
+              byte[] audioData = getFromBuffer();
+              if(audioData != null){
+                System.out.println("Playing");
+                InputStream byteArrayInputStream = new ByteArrayInputStream(
+                        audioData);
+                audioInputStream = new AudioInputStream(byteArrayInputStream,format, audioData.length / format.getFrameSize());
+                DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+                sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+                sourceDataLine.open(format);
+                sourceDataLine.start();
+                int cnt = 0;
+                byte tempBuffer[] = new byte[400];
+                try {
+                    while ((cnt = audioInputStream.read(tempBuffer, 0,tempBuffer.length)) != -1) {
+                        if (cnt > 0) {
+                            // Write data to the internal buffer of
+                            // the data line where it will be
+                            // delivered to the speaker.
+                            sourceDataLine.write(tempBuffer, 0, cnt);
+                        }// end if
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // Block and wait for internal buffer of the
+                // data line to empty.
+                sourceDataLine.drain();
+                sourceDataLine.close();
+              }
+
+            }
+            catch(Exception e){
+              e.printStackTrace();
+            }
+
+          }
+        }
+
     }
 }
---
+++ RecordPlayback.java
@@ -0,0 +1,115 @@
+/*
+ * CO 324 - Network and Web Application Design
+ * Assignment - Sample Code
+ */
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+
+
+
+public class RecordPlayback {
+
+boolean stopCapture = false;
+ByteArrayOutputStream byteArrayOutputStream;
+AudioFormat audioFormat;
+TargetDataLine targetDataLine;
+AudioInputStream audioInputStream;
+SourceDataLine sourceDataLine;
+byte tempBuffer[] = new byte[500];
+
+public static void main(String[] args) {
+
+    RecordPlayback playback = new RecordPlayback();
+    playback.captureAudio();
+
+}
+
+
+
+
+private AudioFormat getAudioFormat() {
+    float sampleRate = 16000.0F;
+    int sampleSizeInBits = 16;
+    int channels = 2;
+    boolean signed = true;
+    boolean bigEndian = true;
+    return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+}
+
+
+
+private void captureAudio() {
+
+    try {
+        Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();    //get available mixers
+        System.out.println("Available mixers:");
+        Mixer mixer = null;
+        for (int cnt = 0; cnt < mixerInfo.length; cnt++) {
+            System.out.println(cnt + " " + mixerInfo[cnt].getName());
+            mixer = AudioSystem.getMixer(mixerInfo[cnt]);
+
+            Line.Info[] lineInfos = mixer.getTargetLineInfo();
+            if (lineInfos.length >= 1 && lineInfos[0].getLineClass().equals(TargetDataLine.class)) {
+                System.out.println(cnt + " Mic is supported!");
+                break;
+            }
+        }
+
+        audioFormat = getAudioFormat();     //get the audio format
+        DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+
+        targetDataLine = (TargetDataLine) mixer.getLine(dataLineInfo);
+        targetDataLine.open(audioFormat);
+        targetDataLine.start();
+
+        DataLine.Info dataLineInfo1 = new DataLine.Info(SourceDataLine.class, audioFormat);
+        sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo1);
+        sourceDataLine.open(audioFormat);
+        sourceDataLine.start();
+
+        //Setting the maximum volume
+        FloatControl control = (FloatControl)sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
+        control.setValue(control.getMaximum());
+
+        captureAndPlay(); //playing the audio
+
+    } catch (LineUnavailableException e) {
+        System.out.println(e);
+        System.exit(0);
+    }
+
+}
+
+    private void captureAndPlay() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        stopCapture = false;
+        try {
+            int readCount;
+            while (!stopCapture) {
+                readCount = targetDataLine.read(tempBuffer, 0, tempBuffer.length);  //capture sound into tempBuffer
+                if (readCount > 0) {
+                    byteArrayOutputStream.write(tempBuffer, 0, readCount);
+                    sourceDataLine.write(tempBuffer, 0, 500);   //playing audio available in tempBuffer
+                }
+            }
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+
+}
